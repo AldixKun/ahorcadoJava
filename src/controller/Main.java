f6b42f0e806022dc678c5ca11f2f9df8e95e7bac
Aldix <aldorodriguezvalverde@gmail.com>
2025-03-04 10:52:20 +0100

    inicio del juego

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,11 +1,38 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("prueba");
-
+		String palabra = "";
+		
+		ArrayList <String> palabras = new ArrayList <String>();
+		
+		palabras.add("Melon");
+		palabras.add("Sandia");
+		palabras.add("Platano");
+		palabras.add("Kiwi");
+		
+		ArrayList <String> blanco = new ArrayList <String>();
+	
+		
+		Random random= new Random();
+		
+		palabra= palabras.get(random.nextInt(palabras.size())) ;
+		
+		
+		char[] caracteresPalabras = palabra.toCharArray();
+		char[] caracteresInvertidos = palabra.toCharArray();
+		for (int i = 0; i< caracteresPalabras.length; i++) {
+		blanco.add("_");
+		
+		}
+		
+		System.out.println(blanco);
+		System.out.println(palabra);
 	}
 
 }
